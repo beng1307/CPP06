444d4d6ea248896d50b7afd5a641cef975cb7e8e
Benjamin Gretic <bgretic@c3r9p1.42vienna.com>
2026-04-23 14:27:49 +0200

    CPP06 done

--- ex02/Base.hpp
+++ ex02/Base.hpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Base.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bgretic <bgretic@student.42vienna.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/23 14:25:28 by bgretic           #+#    #+#             */
+/*   Updated: 2026/04/23 14:25:29 by bgretic          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef BASE_HPP
 # define BASE_HPP
 
@@ -8,8 +20,8 @@ class Base
 		virtual ~Base();
 
 		static Base	*generate(void);
-		static void	identify(Base* p);
-		static void	identify(Base& p);
+		static void	identify(Base *p);
+		static void	identify(Base &p);
 };
 
 #endif
